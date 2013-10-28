39294659cc1807f178d9d6491a31e9a2d54d51ea
Thorben Stangenberg <thorben.stangenberg@inoft.com>
2013-10-28 16:32:13 +0100

    added collectd

--- Vagrantfile
+++ Vagrantfile
@@ -8,6 +8,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/precise/current/precise-server-cloudimg-i386-vagrant-disk1.box"
   config.vm.customize ["modifyvm", :id, "--ioapic", "on", "--memory", "2048", "--cpus", "2"]
   config.vm.forward_port 80, 9000
+  config.vm.forward_port 2003, 2003
+  config.vm.host_name = "graphite"
   # config.vm.provision :puppet, :module_path => "modules" do |puppet|
   config.vm.provision :puppet, :options => "--verbose --debug", :module_path => "modules" do |puppet|
     puppet.manifests_path = "manifests"
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,13 +1,53 @@
+include apt
+
+# this repo is needed vor collectd version 5.3
+apt::ppa { "ppa:vbulax/collectd5": }
+
+exec { "apt-update":
+	command => '/usr/bin/apt-get update',
+	user => root,
+	require => Apt::Ppa["ppa:vbulax/collectd5"],
+}
+
 # works currently only on port 80 due to setup issues with ports.conf
 class { 'graphite':
   port => 80,
+  require => Exec["apt-update"],
 }
 
 # add all NameVirtualHost directives to the ports.conf
 # @see https://forge.puppetlabs.com/puppetlabs/apache#configure-a-virtual-host
 # class { 'apache::namevirtualhost': }
 
-include apache
+class { '::apache':
+	require => Exec["apt-update"],
+}
+
+class { '::collectd':
+  require => Exec["apt-update"],
+}
+
+include apache 
 
 include graphite
 
+include collectd
+
+collectd::plugin { 'cpu': }
+collectd::plugin { 'df': }
+
+class { 'collectd::plugin::disk':
+  disks          => ['/^dm/'],
+  ignoreselected => true
+}
+
+collectd::plugin { 'entropy': }
+collectd::plugin { 'interface': }
+collectd::plugin { 'irq': }
+collectd::plugin { 'iptables': }
+collectd::plugin { 'memory': }
+collectd::plugin { 'processes': }
+
+class { 'collectd::plugin::write_graphite':
+  graphitehost => 'localhost',
+}
